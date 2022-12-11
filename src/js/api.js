57e7ef27a4673c8be2916311a3959239c001d19f
JuliaKravchenko8 <107355879+JuliaKravchenko8@users.noreply.github.com>
2022-12-11 21:52:24 +0200

    add files

--- src/js/api.js
+++ src/js/api.js
@@ -1,24 +1,24 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const API_KEY = '31983214-2696166acee282192c021d74b';
-const URL = 'https://pixabay.com/api';
+// // const API_KEY = '31983214-2696166acee282192c021d74b';
+// // const URL = 'https://pixabay.com/api';
 
-export class API {
-  constructor(query) {
-    this.query = query;
-    this.page = 1;
-    this.perPage = 40;
-    this.axios = require('axios');
-  }
+// export class API {
+//   constructor(query) {
+//     this.URL = 'https://pixabay.com/api/';
+//     this.API_KEY = '31983214-2696166acee282192c021d74b';
+//     this.query = query;
+//     this.page = 1;
+//   }
 
-  async getPhotos() {
-    const promise = await axios.get(
-      `${URL}?key=${API_KEY}&q=${this.query}&image_type=photo&safesearch=true&orientation=horizontal&page=${this.page}&per_page=$(this.perPage)`
-    );
-    return promise;
-  }
+//   async getPhotos() {
+//     const promise = await axios.get(
+//       `${this.URL}?key=${this.API_KEY}&q=${this.query}&image_type=photo&safesearch=true&orientation=horizontal&page=${this.page}&per_page=40`
+//     );
+//     return promise;
+//   }
 
-  pageIncrement() {
-    this.page += 1;
-  }
-}
+//   pageIncrement() {
+//     this.page += 1;
+//   }
+// }
