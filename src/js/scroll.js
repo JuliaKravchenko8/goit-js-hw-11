4ea3e8df46791cf6d1fbd626a2504f101475c8c3
JuliaKravchenko8 <107355879+JuliaKravchenko8@users.noreply.github.com>
2022-12-11 22:31:05 +0200

    delete folder

--- src/js/scroll.js
+++
@@ -1,9 +0,0 @@
-// export function smoothScroll(gallery) {
-//   const { height: cardHeight } =
-//     gallery.firstElementChild.getBoundingClientRect();
-
-//   window.scrollBy({
-//     top: cardHeight * 1.7,
-//     behavior: 'smooth',
-//   });
-// }
