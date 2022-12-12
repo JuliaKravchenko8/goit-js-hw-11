6c255b25d3bdecd8d26cc340961b7414dcd23508
JuliaKravchenko8 <107355879+JuliaKravchenko8@users.noreply.github.com>
2022-12-12 23:32:15 +0200

    destructuring

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import { createPhotosHtml } from './js/image-create';
+import scroll from './js/scroll';
 
 const API = '31983214-2696166acee282192c021d74b';
 
@@ -9,7 +11,7 @@ const searchForm = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
 const buttonLoadMore = document.querySelector('.load-more');
 
-const gal = new SimpleLightbox('.gallery a', {
+const lightbox = new SimpleLightbox('.gallery a', {
   captions: true,
   captionDelay: 250,
 });
@@ -61,65 +63,29 @@ function loadMore() {
   getPictures(queryObj.returnUrl());
 }
 
-function createPhotosHtml(imagesArr) {
-  return imagesArr
-    .map(image => {
-      return `
-<div class="photo-card">
-  <a class="image-link" href="${image.largeImageURL}"><img src="${image.webformatURL}" alt="${image.tags}"  loading="lazy" /></a>
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b>
-      <span class="count">${image.likes}</span>
-    </p>
-    <p class="info-item">
-      <b>Views</b>
-      <span class="count">${image.views}</span>
-    </p>
-    <p class="info-item">
-      <b>Comments</b>
-      <span class="count">${image.comments}</span>
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>
-      <span class="count">${image.downloads}</span>
-    </p>
-  </div>
-</div>`;
-    })
-    .join('');
-}
-
-function scroll() {
-  if (queryObj.page !== 1) {
-    let { height: cardHeight } = document
-      .querySelector('.gallery')
-      .firstElementChild.getBoundingClientRect();
-
-    window.scrollBy({
-      top: cardHeight * 2,
-      behavior: 'smooth',
-    });
-  }
-}
-
 async function getPictures(url) {
   try {
     const response = await axios.get(url);
     const images = response.data.hits;
+    // let totalPages = Math.ceil(totalHits / 40);
+
     if (images.length === 0) {
       Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
       );
       return;
     }
+    // if (totalPages <= page) {
+    //   Notify.info("We're sorry, but you've reached the end of search results.");
+    //   // buttonLoadMore.classList.add('block');
+    // }
     if (queryObj.page === 1) {
       Notify.info(`Hooray! We found ${response.data.totalHits} images.`);
     }
     buttonLoadMoreState('block');
     gallery.insertAdjacentHTML('beforeend', createPhotosHtml(images));
     scroll();
-    gal.refresh();
+    lightbox.refresh();
   } catch (error) {
     Notify.failure(error);
   }
---
+++ src/js/scroll.js
@@ -0,0 +1,12 @@
+export function scroll() {
+  if (queryObj.page !== 1) {
+    let { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+  }
+}
