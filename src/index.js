c5718ad1310ac662baa857576b409957ac5d2a7d
JuliaKravchenko8 <107355879+JuliaKravchenko8@users.noreply.github.com>
2022-12-13 11:55:45 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import { createPhotosHtml } from './js/image-create';
-import { scroll } from './js/scroll';
 
 const API = '31983214-2696166acee282192c021d74b';
 
@@ -15,6 +13,36 @@ const lightboxGallery = new SimpleLightbox('.gallery a', {
   captions: true,
   captionDelay: 250,
 });
+
+function createPhotosHtml(imagesArr) {
+  return imagesArr
+    .map(image => {
+      return `
+<div class="photo-card">
+  <a class="image-link" href="${image.largeImageURL}"><img src="${image.webformatURL}" alt="${image.tags}"  loading="lazy" /></a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+      <span class="count">${image.likes}</span>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+      <span class="count">${image.views}</span>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+      <span class="count">${image.comments}</span>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+      <span class="count">${image.downloads}</span>
+    </p>
+  </div>
+
+</div>`;
+    })
+    .join('');
+}
 
 const fetchImages = {
   perPage: 40,
@@ -63,13 +91,26 @@ function loadMore() {
   getPictures(fetchImages.returnUrl());
 }
 
+function scroll() {
+  if (fetchImages.page !== 1) {
+    let { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+  }
+}
+
 async function getPictures(url) {
   try {
     const response = await axios.get(url);
     const images = response.data.hits;
 
     // delete load more
-    let totalPages = Math.ceil(response.data.totalHits / 40);
+    let totalPages = Math.floor(response.data.totalHits / 40);
     if (images.length <= totalPages) {
       buttonLoadMoreState('none');
       return;
