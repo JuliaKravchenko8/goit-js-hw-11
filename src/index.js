0ae9482dfe42b25f870d3c714abed65d51cef372
JuliaKravchenko8 <107355879+JuliaKravchenko8@users.noreply.github.com>
2022-12-14 01:33:30 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,155 +1,96 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchImages } from './js/fetch-images';
+import { getImageTemplate } from './js/image-markup';
+import { smoothScroll } from './js/smooth-scroll';
+
 import SimpleLightbox from 'simplelightbox';
+import { Notify } from 'notiflix';
+
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import './sass/index.scss';
 
-const axios = require('axios').default;
-const APIKEY = '30622071-b7f240b19c0374f09e6aee33e';
-const searchForm = document.querySelector('#search-form');
-const gallery = document.querySelector('.gallery');
-const buttonLoadMore = document.querySelector('.load-more');
-
-const gal = new SimpleLightbox('.gallery a', {
-  captions: true,
-  captionDelay: 250,
-});
-
-const queryObj = {
-  perPage: 40,
-  query: '',
-  page: 1,
-  returnUrl() {
-    return `https://pixabay.com/api/?key=${APIKEY}&q=${this.query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${this.perPage}&page=${this.page}`;
-  },
-  nextPage() {
-    this.page += 1;
-  },
-  setQuery(query) {
-    this.query = query;
-  },
-  setPage(page) {
-    this.page = page;
-  },
+const refs = {
+  searchForm: document.querySelector('.search-form'),
+  gallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+  formInput: document.querySelector('[name="searchQuery"]'),
 };
 
-function createPhotosHtml(imagesArr) {
-  return imagesArr
-    .map(image => {
-      return `
-<div class="photo-card">
-  <a class="image-link" href="${image.largeImageURL}"><img src="${image.webformatURL}" alt="${image.tags}"  loading="lazy" /></a>
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b>
-      <span class="count">${image.likes}</span>
-    </p>
-    <p class="info-item">
-      <b>Views</b>
-      <span class="count">${image.views}</span>
-    </p>
-    <p class="info-item">
-      <b>Comments</b>
-      <span class="count">${image.comments}</span>
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>
-      <span class="count">${image.downloads}</span>
-    </p>
-  </div>
+let page = 1;
+const per_page = 40;
+let searchQuery = null;
+let images = [];
+let totalPages = 0;
 
-</div>`;
-    })
-    .join('');
-}
+refs.formInput.setAttribute('required', true);
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
-function clearGallery() {
-  gallery.innerHTML = '';
-}
+async function onSearch(e) {
+  e.preventDefault();
 
-function buttonLoadMoreState(param) {
-  buttonLoadMore.style.display = param;
-}
+  searchQuery = e.currentTarget.searchQuery.value.trim();
 
-function searchPictures(event) {
-  buttonLoadMoreState('none');
-  clearGallery();
-  event.preventDefault();
-  const query = event.target.searchQuery.value.trim();
-  if (query === '') {
+  if (searchQuery === '') {
+    cleanMarkup();
+    lockLoadMoreBtn();
+    Notify.warning('Please enter your query');
     return;
   }
-  queryObj.setPage(1);
-  queryObj.setQuery(query);
-  getPictures(queryObj.returnUrl());
-}
 
-searchForm.addEventListener('submit', searchPictures);
-buttonLoadMore.addEventListener('click', loadMore);
+  page = 1;
 
-function loadMore() {
-  queryObj.nextPage();
-  getPictures(queryObj.returnUrl());
-}
+  const response = await fetchImages(searchQuery, page, per_page);
+  const totalImages = response.data.totalHits;
+  images = response.data.hits;
+  totalPages = totalImages / per_page;
 
-function createPhotosHtml(imagesArr) {
-  return imagesArr
-    .map(image => {
-      return `
-<div class="photo-card">
-  <a class="image-link" href="${image.largeImageURL}"><img src="${image.webformatURL}" alt="${image.tags}"  loading="lazy" /></a>
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b>
-      <span class="count">${image.likes}</span>
-    </p>
-    <p class="info-item">
-      <b>Views</b>
-      <span class="count">${image.views}</span>
-    </p>
-    <p class="info-item">
-      <b>Comments</b>
-      <span class="count">${image.comments}</span>
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>
-      <span class="count">${image.downloads}</span>
-    </p>
-  </div>
-</div>`;
-    })
-    .join('');
-}
-
-function scroll() {
-  if (queryObj.page !== 1) {
-    let { height: cardHeight } = document
-      .querySelector('.gallery')
-      .firstElementChild.getBoundingClientRect();
-
-    window.scrollBy({
-      top: cardHeight * 2,
-      behavior: 'smooth',
-    });
+  if (images.length > 0) {
+    Notify.success(`Hooray! We found ${totalImages} images.`);
+    unlockLoadMoreBtn();
+  } else {
+    Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    lockLoadMoreBtn();
   }
+  cleanMarkup();
+  render();
 }
 
-async function getPictures(url) {
-  try {
-    const response = await axios.get(url);
-    const images = response.data.hits;
-    if (images.length === 0) {
-      Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-      return;
-    }
-    if (queryObj.page === 1) {
-      Notify.info(`Hooray! We found ${response.data.totalHits} images.`);
-    }
-    buttonLoadMoreState('block');
-    gallery.insertAdjacentHTML('beforeend', createPhotosHtml(images));
-    scroll();
-    gal.refresh();
-  } catch (error) {
-    Notify.failure(error);
+async function onLoadMore(e) {
+  page += 1;
+  const response = await fetchImages(searchQuery, page, per_page);
+  images = response.data.hits;
+  const totalImages = response.data.totalHits;
+  totalPages = totalImages / per_page;
+
+  if (totalPages <= page) {
+    Notify.info("We're sorry, but you've reached the end of search results.");
+    lockLoadMoreBtn();
   }
+  render();
+  smoothScroll('.gallery');
+}
+
+function render() {
+  const items = images;
+
+  const galleryMarkup = items.map(getImageTemplate);
+
+  refs.gallery.insertAdjacentHTML('beforeend', galleryMarkup.join(''));
+
+  const lightbox = new SimpleLightbox('.gallery a');
+  lightbox.refresh();
+}
+
+function lockLoadMoreBtn() {
+  refs.loadMoreBtn.classList.add('hidden');
+}
+
+function unlockLoadMoreBtn() {
+  refs.loadMoreBtn.classList.toggle('hidden', totalPages <= page);
+}
+
+function cleanMarkup() {
+  refs.gallery.innerHTML = '';
 }
